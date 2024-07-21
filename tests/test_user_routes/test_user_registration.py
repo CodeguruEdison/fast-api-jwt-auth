84d965a4977a1ac4bf687abf99a328d612da37a5
Ayaskanta <ayaskantasamal@Ayaskantas-MacBook-Pro.local>
2024-07-21 07:10:27 -0400

    [07-21-2024] - test and workflow added

--- tests/test_user_routes/test_user_registration.py
+++ tests/test_user_routes/test_user_registration.py
@@ -2,6 +2,7 @@ from tests.conftest import USER_NAME,USER_EMAIL,USER_PASSWORD
 # docker-compose run test-service
 #https://github.com/Describly/fastapi-sqlalchemy-alembic?tab=readme-ov-file
 #https://www.youtube.com/watch?v=7_3TnuQ4EVY&t=381s
+
 def test_create_user(client):
     data ={
         "name":USER_NAME,
